1b6dc80b051842289a90538bec0ee90d7517d15a
Tom <93992802+0wme@users.noreply.github.com>
2024-11-06 19:32:26 +0100

    Creation of EDTPage and ServicePage

---
+++ resources/js/Pages/EDTPage.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="page-content">
+      <Sidebar />
+      <div class="main-content">
+        <<!-- Contenu de la page EDT -->>
+      </div>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  import Sidebar from '../Components/Navigation/Sidebar.vue';
+  </script>
+  
+  <style scoped>
+  /* Styles identiques */
+  </style>
