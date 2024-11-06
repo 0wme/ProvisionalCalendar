9ad9838a3f0e55f1f3b61344c4da88abe6c58d02
Smimid Sofian <sofian.smimid@etu.unilim.fr>
2024-11-06 16:37:00 +0100

    add of a login page and a result page who change on differtents roles of the connected user

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,12 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
+
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::get('/result', [AuthController::class, 'result'])
+    ->name('result')
+    ->middleware('auth');
+
 Route::get('/', function () {
     return Inertia::render('LoginPage');
 });
