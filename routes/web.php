a974f9289b8d193c3cf3751503df886394a3878d
Tom <93992802+0wme@users.noreply.github.com>
2024-11-06 16:10:18 +0100

    SideBar and SideBarItem creation

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Home');
+    return Inertia::render('TeachersTeachingsPageContent');
 });
 
 Route::post('/login', [AuthController::class, 'login'])->name('login');
