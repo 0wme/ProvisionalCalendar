95d9d49daec092ac666e06d9d4c441112eb1a256
Tom <93992802+0wme@users.noreply.github.com>
2024-11-06 16:10:18 +0100

    SideBar and SideBarItem creation

---
+++ resources/js/Components/Navigation/SidebarItem.vue
@@ -0,0 +1,28 @@
+<template>
+    <li class="sidebar-item">
+      <component :is="icon" class="icon" />
+      <span>{{ label }}</span>
+    </li>
+  </template>
+  
+  <script setup lang="ts">
+  import { defineProps } from 'vue';
+  
+  const props = defineProps({
+    icon: Object,
+    label: String,
+  });
+  </script>
+  
+  <style scoped>
+  .sidebar-item {
+    display: flex;
+    align-items: center;
+    padding: 10px;
+    cursor: pointer;
+  }
+  
+  .icon {
+    margin-right: 10px;
+  }
+  </style>
