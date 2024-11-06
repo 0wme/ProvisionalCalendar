51d43153d85e7d6bb21a7e683fe114b149d690e4
Tom <93992802+0wme@users.noreply.github.com>
2024-11-06 16:10:00 +0100

    Creating the TeachersTeachingsPageContent.vue page

---
+++ resources/js/Pages/TeachersTeachingsPageContent.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="page-content">
+      <Sidebar />
+      <div class="main-content">
+      </div>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  import Sidebar from '../Components/Navigation/Sidebar.vue';
+  </script>
+  
+  <style scoped>
+  .page-content {
+    display: flex;
+  }
+  
+  .main-content {
+    flex-grow: 1;
+    padding: 20px;
+  }
+  </style>
