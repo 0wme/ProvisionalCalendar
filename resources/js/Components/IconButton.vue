32b1d8487f97c7bac5b4cdba8a0d41dec73645c3
Rémi Blanchard <remi.blanchard1@etu.unilim.fr>
2024-11-06 16:41:10 +0100

    add working IconButton

--- resources/js/Components/IconButton.vue
+++ resources/js/Components/IconButton.vue
@@ -1,47 +1,42 @@
+<script setup lang="ts">
+
+import { Pen } from 'lucide-vue-next';
+const props = defineProps<{
+  iconClass: string
+}>()
+</script>
+
 <template>
   <button 
     class="icon-button"
     @click="$emit('click')"
-  >
-    <i class="icon" :class="iconClass"></i>
-    <span v-if="label">{{ label }}</span>
+    >
+    <Pen size="32" /> 
+    <Icon :name="iconClass" />
   </button>
 </template>
-
-<script>
-export default {
-  name: 'IconButton',
-  props: {
-    iconClass: {
-      type: String,
-      required: true
-    },
-    label: {
-      type: String,
-      default: ''
-    }
-  }
-}
-</script>
 
 <style scoped>
 .icon-button {
   display: flex;
+  justify-content: center;
   align-items: center;
-  gap: 8px;
-  padding: 8px 16px;
+  width: 80px;
+  height: 80px;
+  padding: 0;
   border: none;
-  border-radius: 4px;
+  margin: 100px;
+  border-radius: 20px;
   cursor: pointer;
-  background-color: #4CAF50;
-  color: white;
+  background-color: #ffc0cb;
+  color: black;
 }
 
 .icon-button:hover {
-  background-color: #45a049;
+  filter: brightness(0.8);
 }
 
 .icon {
-  font-size: 18px;
+  font-size: 35px;
 }
 </style>
