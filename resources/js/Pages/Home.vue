f99776d62efcd11df377e629d4b0a053f357ac85
Tom <93992802+0wme@users.noreply.github.com>
2024-11-06 15:24:31 +0100

    test

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -12,48 +12,8 @@ const submit = () => {
 </script>
 
 <template>
-    <Head title="Connexion" />
-    <div class="min-h-screen flex items-center justify-center">
-        <div class="w-full max-w-md">
-            <form @submit.prevent="submit" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-                <div class="mb-4">
-                    <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
-                        Identifiant
-                    </label>
-                    <input 
-                        v-model="form.username"
-                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        id="username"
-                        type="text"
-                        placeholder="Identifiant"
-                    >
-                    
-                </div>
-                <div class="mb-6">
-                    <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
-                        Mot de passe
-                    </label>
-                    <input 
-                        v-model="form.password"
-                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-                        id="password"
-                        type="password"
-                        placeholder="******************"
-                    >
-                </div>
-                <div v-if="form.errors.username" class="text-red-500 text-xs mt-1">
-                        {{ form.errors.username }}
-                </div>
-                <div class="flex items-center justify-center">
-                    <button 
-                        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                        type="submit"
-                        :disabled="form.processing"
-                    >
-                        Se connecter
-                    </button>
-                </div>
-            </form>
-        </div>
+    <Head title="Calendrier prévisionnel" />
+    <div class="container">
+        <h1 class="text-center">Helloo world!</h1>
     </div>
 </template>
