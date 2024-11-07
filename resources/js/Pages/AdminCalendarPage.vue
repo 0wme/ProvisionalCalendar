3881d48033938f3c855ac50d54a42d00aa87afe2
Tom <93992802+0wme@users.noreply.github.com>
2024-11-07 10:22:11 +0100

    Creation of HeaderMenu and implementation about it

--- resources/js/Pages/AdminCalendarPage.vue
+++ resources/js/Pages/AdminCalendarPage.vue
@@ -1,16 +1,17 @@
 <template>
-  <div class="page-content">
-    <Sidebar />
-    <div class="main-content">
-        <h1>Calendrier Prévisionnel</h1>
+    <div class="page-content">
+      <HeaderMenu />
+      <Sidebar />
+      <div class="main-content">
+      </div>
     </div>
-  </div>
-</template>
-
-<script setup lang="ts">
-import Sidebar from '../Components/Navigation/Sidebar.vue';
-</script>
-
-<style scoped>
-
-</style>
+  </template>
+  
+  <script setup lang="ts">
+  import Sidebar from '../Components/Navigation/Sidebar.vue';
+  import HeaderMenu from '../Components/Navigation/HeaderMenu.vue';
+  </script>
+  
+  <style scoped>
+  /* Ajoutez des styles si nécessaire */
+  </style>
