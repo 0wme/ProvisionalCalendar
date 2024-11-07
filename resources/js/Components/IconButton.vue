58cb65372481f416b5d373c7ea5b5729649d1e84
maelreinertmartinez <111981856+maelreinertmartinez@users.noreply.github.com>
2024-11-07 11:34:43 +0100

    Fix button size

--- resources/js/Components/IconButton.vue
+++ resources/js/Components/IconButton.vue
@@ -11,7 +11,7 @@ const props = defineProps<{
 
 <template>
   <button 
-    :class="['icon-button flex justify-center items-center cursor-pointer text-black hover:brightness-75', small ? 'w-8 h-8 rounded-full' : 'w-12 h-12 rounded-xl', { 'shadow-md': props.hasShadow }]"
+    :class="['icon-button flex justify-center items-center cursor-pointer text-black hover:brightness-75', small ? 'p-2 rounded-full' : 'p-4 rounded-xl', { 'shadow-md': props.hasShadow }]"
     :style="{ '--bg-color': props.bgColor }"
   >
     <Icon :name="iconClass" :size="small ? 16 : 24" />
