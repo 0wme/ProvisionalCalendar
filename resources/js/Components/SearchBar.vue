7e955fcaa14ed880d5a8b840748605c48bb47f8f
maelreinertmartinez <111981856+maelreinertmartinez@users.noreply.github.com>
2024-11-07 09:53:07 +0100

    adding of searchbar

--- resources/js/Components/SearchBar.vue
+++ resources/js/Components/SearchBar.vue
@@ -3,9 +3,9 @@ import Icon from '@/Components/Icon.vue';
 </script>
 
 <template>
-    <div>
+    <div class="search-bar flex items-center gap-2 border border-gray-300 rounded-2xl p-2">
         <Icon name="Search" />
-        <input type="text" placeholder="Rechercher" />
+        <input class="border-none focus:ring-0 w-full" type="text" placeholder="Rechercher" />
     </div>
 </template>
 
