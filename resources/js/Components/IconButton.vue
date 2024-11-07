7fc94a0453ff2ed0223f00f222ec5e087abd552e
Rémi Blanchard <remi.blanchard1@etu.unilim.fr>
2024-11-07 11:14:52 +0100

    commit IconButton ( with props )

--- resources/js/Components/IconButton.vue
+++ resources/js/Components/IconButton.vue
@@ -1,15 +1,25 @@
 <script setup lang="ts">
-
 import Icon from '@/Components/Icon.vue';
+
 const props = defineProps<{
-  iconClass: string
+  iconClass: string,
+  bgColor?: string,
+  small?: boolean,
+  hasShadow?: boolean
 }>()
 </script>
 
 <template>
   <button 
-    class="flex justify-center items-center w-20 h-20 rounded-[20px] cursor-pointer bg-pink-200 text-black hover:brightness-75"
-    >
-    <Icon :name="iconClass" class="text-4xl"  />
+    :class="['icon-button flex justify-center items-center cursor-pointer text-black hover:brightness-75', small ? 'w-8 h-8 rounded-full' : 'w-12 h-12 rounded-xl', { 'shadow-md': props.hasShadow }]"
+    :style="{ '--bg-color': props.bgColor }"
+  >
+    <Icon :name="iconClass" :size="small ? 16 : 24" />
   </button>
 </template>
+
+<style scoped>
+.icon-button {
+  background-color: var(--bg-color);
+}
+</style>
