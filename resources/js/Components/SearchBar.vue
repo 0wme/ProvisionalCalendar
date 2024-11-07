6dead6ede9f9a25f46221b9c111efc5dbc773e3f
maelreinertmartinez <111981856+maelreinertmartinez@users.noreply.github.com>
2024-11-07 11:16:43 +0100

    updating searchbar

--- resources/js/Components/SearchBar.vue
+++ resources/js/Components/SearchBar.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
 import Icon from '@/Components/Icon.vue';
+import IconButton from '@/Components/IconButton.vue';
+
+const props = defineProps<{
+    placeholder: string
+}>()
 </script>
 
 <template>
-    <div class="search-bar flex items-center gap-2 border border-gray-300 rounded-2xl p-2">
+    <div class="search-bar flex items-center gap-2 border border-gray-300 rounded-2xl p-1">
         <Icon name="Search" />
-        <input class="border-none focus:ring-0 w-full" type="text" placeholder="Rechercher" />
+        <input class="border-none focus:ring-0 w-full" type="text" :placeholder="placeholder" />
+        <IconButton v-for="i in 2" :key="i" iconClass="Pen" />
     </div>
 </template>
 
