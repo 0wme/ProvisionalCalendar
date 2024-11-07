9d00bba77bad3440aa08ce86d58f6d92046558c4
Tom <93992802+0wme@users.noreply.github.com>
2024-11-07 10:35:37 +0100

    Header Page added and routing of them

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,19 @@ Route::get('/edt', function () {
 Route::get('/service', function () {
     return Inertia::render('ServicePage');
 })->name('service');
+
+Route::get('/groupes', function () {
+    return Inertia::render('GroupPage');
+})->name('groupes');
+
+Route::get('/alertes', function () {
+    return Inertia::render('AlertPage');
+})->name('alertes');
+
+Route::get('/configurations', function () {
+    return Inertia::render('ConfigPage');
+})->name('configurations');
+
+Route::get('/enseignants', function () {
+    return Inertia::render('TeachersTeachingsPageContent');
+})->name('enseignants');
