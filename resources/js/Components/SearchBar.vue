6a94e49c790773c606f9d81d4ad789374e5a7055
maelreinertmartinez <111981856+maelreinertmartinez@users.noreply.github.com>
2024-11-07 09:53:07 +0100

    adding of searchbar

---
+++ resources/js/Components/SearchBar.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import Icon from '@/Components/Icon.vue';
+</script>
+
+<template>
+    <div>
+        <Icon name="Search" />
+        <input type="text" placeholder="Rechercher" />
+    </div>
+</template>
+
+<style scoped>
+
+</style>
