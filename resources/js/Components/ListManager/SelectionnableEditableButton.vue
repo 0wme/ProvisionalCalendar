171370399d03f5eaf5bea5763731e6634ff9ced2
maelreinertmartinez <111981856+maelreinertmartinez@users.noreply.github.com>
2024-11-08 09:29:58 +0100

    adding of selectionnable button editable

---
+++ resources/js/Components/ListManager/SelectionnableEditableButton.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import SelectionnableButton from './SelectionnableButton.vue';
+import IconButton from '@/Components/IconButton.vue';
+
+const props = defineProps<{
+    item: any,
+    selected: boolean
+}>()
+
+const emit = defineEmits(['select']);
+
+const handleEdit = () => {
+    // TODO: Ouverture de la popup
+    console.log('edit');
+}
+
+const handleSelect = () => {
+    emit('select', props.item.id);
+}
+</script>
+
+<template>
+    <div class="selectionnable-button-editable flex items-center gap-2">
+        <SelectionnableButton class="w-full" :name="item.name" :selected @click="handleSelect" />
+        <IconButton iconClass="Pencil" bgColor="#E8DEF8" @click="handleEdit" hasShadow />
+    </div>
+</template>
