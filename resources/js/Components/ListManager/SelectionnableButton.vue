e48cdfcd05fbab6a0a0ef739aef81fc312a0a844
maelreinertmartinez <111981856+maelreinertmartinez@users.noreply.github.com>
2024-11-08 11:28:00 +0100

    fix selectionnable button text position

--- resources/js/Components/ListManager/SelectionnableButton.vue
+++ resources/js/Components/ListManager/SelectionnableButton.vue
@@ -6,7 +6,7 @@ defineProps<{
 </script>
 
 <template>
-    <button :class="['selectionnable-button rounded-2xl p-4 shadow-lg', selected ? 'bg-red-100 border border-red-600 border-2' : 'bg-gray-100']">
+    <button :class="['selectionnable-button rounded-2xl p-4 shadow-lg text-left', selected ? 'bg-red-100 border border-red-600 border-2' : 'bg-gray-100']">
         <p>{{ name }}</p>
     </button>
 </template>
