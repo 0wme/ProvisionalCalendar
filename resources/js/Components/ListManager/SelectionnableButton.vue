c09b54c84f56b08625bd3ee63b7be0bbdbbc8274
maelreinertmartinez <111981856+maelreinertmartinez@users.noreply.github.com>
2024-11-08 09:29:49 +0100

    adding of selectionnable button

---
+++ resources/js/Components/ListManager/SelectionnableButton.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+defineProps<{
+    name: string,
+    selected: boolean
+}>()
+</script>
+
+<template>
+    <button :class="['selectionnable-button rounded-2xl p-4 shadow-lg', selected ? 'bg-red-100 border border-red-600 border-2' : 'bg-gray-100']">
+        <p>{{ name }}</p>
+    </button>
+</template>
