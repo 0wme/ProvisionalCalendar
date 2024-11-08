aaa8ea0c135af399cb637ef7e8411913cd7192b7
Tom <93992802+0wme@users.noreply.github.com>
2024-11-08 14:42:10 +0100

    Creation of ClassListManager, GroupList and SubGroup + imp

---
+++ resources/js/Features/ListManager/GroupListManager.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import ListManager from '@/Components/ListManager/ListManager.vue';
+import { defineProps, defineEmits } from 'vue';
+import { Item } from '@/types/models';
+
+defineProps<{
+  title: string;
+  groups: Item[];
+}>();
+
+const emit = defineEmits(['select']);
+
+const handleSelect = (item: Item) => {
+  emit('select', item);
+}
+</script>
+
+<template>
+  <ListManager
+    title="Groupes"
+    hasAdd
+    :items="groups"
+    @select="handleSelect"
+  />
+</template>
