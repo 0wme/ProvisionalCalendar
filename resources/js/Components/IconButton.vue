af453c18f15609550c992276b441fb8953b977df
Emilien <emilienrx0@gmail.com>
2024-11-12 10:12:14 +0100

    add components

---
+++ resources/js/Components/IconButton.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import Icon from '@/Components/Icon.vue';
+
+defineProps<{
+  iconClass: string,
+  bgColor?: string,
+  iconColor?: string,
+  small?: boolean,
+  hasShadow?: boolean
+}>()
+</script>
+
+<template>
+  <button 
+    :class="[
+      'icon-button flex justify-center items-center cursor-pointer text-black hover:brightness-75',
+      small ? 'p-2 rounded-full' : 'p-4 rounded-xl',
+      { 'shadow-md': hasShadow }
+    ]"
+    :style="{ 'background-color': bgColor }"
+  >
+    <Icon :name="iconClass" :size="small ? 16 : 24" :color="iconColor" stroke-width="2" />
+  </button>
+</template>
---
+++ resources/js/Components/SelectionnableEditableButton.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import SelectionnableButton from './SelectionnableButton.vue';
+import IconButton from '@/Components/IconButton.vue';
+import { Item } from '@/types/models';
+
+const props = defineProps<{
+    item: Item,
+    selected?: boolean
+}>()
+
+const emit = defineEmits(['select']);
+
+const handleEdit = () => {
+    // TODO: Ouverture de la popup
+    console.log('edit');
+}
+
+const handleSelect = () => {
+    emit('select', props.item.id);
+}
+</script>
+
+<template>
+    <div class="selectionnable-button-editable flex items-center gap-2">
+        <SelectionnableButton class="w-full" :name="item.name" :selected @click="handleSelect" />
+        <IconButton iconClass="Pencil" bgColor="#E8DEF8" @click="handleEdit" hasShadow />
+    </div>
+</template>
