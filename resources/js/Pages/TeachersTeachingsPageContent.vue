f787c77f41e56fa79e941d837978882e8b7162bf
Emilien <emilienrx0@gmail.com>
2024-11-12 10:30:56 +0100

    refactoring

--- resources/js/Pages/TeachersTeachingsPageContent.vue
+++ resources/js/Pages/TeachersTeachingsPageContent.vue
@@ -4,6 +4,8 @@
     <Sidebar />
     <div class="main-content">
       <!-- Contenu de la page Enseignants/Enseignements -->
+      <TeachersListManager />
+      <TeachingsListManager />
     </div>
   </div>
 </template>
@@ -11,6 +13,8 @@
 <script setup lang="ts">
 import Sidebar from '../Components/Navigation/Sidebar.vue';
 import HeaderMenu from '../Components/Navigation/HeaderMenu.vue';
+import TeachersListManager from '../Components/Features/ListManager/TeachersListManager.vue';
+import TeachingsListManager from '../Components/Features/ListManager/TeachingsListManager.vue';
 </script>
 
 <style scoped>
--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,31 @@ use Inertia\Inertia;
 Route::get('/', function () {
     return Inertia::render('Home');
 });
+
+Route::get('/admin-calendar', function () {
+    return Inertia::render('AdminCalendarPage');
+})->name('admin-calendar');
+
+Route::get('/edt', function () {
+    return Inertia::render('EDTPage');
+})->name('edt');
+
+Route::get('/service', function () {
+    return Inertia::render('ServicePage');
+})->name('service');
+
+Route::get('/groupes', function () {
+    return Inertia::render('GroupPage');
+})->name('groupes');
+
+Route::get('/alertes', function () {
+    return Inertia::render('AlertPage');
+})->name('alertes');
+
+Route::get('/configurations', function () {
+    return Inertia::render('ConfigPage');
+})->name('configurations');
+
+Route::get('/enseignants', function () {
+    return Inertia::render('TeachersTeachingsPageContent');
+})->name('enseignants');
