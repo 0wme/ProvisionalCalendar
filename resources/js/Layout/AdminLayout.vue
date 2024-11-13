83899b88aab197528cba2f615f23afc1851c8dba
Mael REINERT MARTINEZ <mael@reinertmartinez.fr>
2024-11-13 11:38:04 +0100

    adding of layouts

--- resources/js/Layout/AdminLayout.vue
+++ resources/js/Layout/AdminLayout.vue
@@ -1,12 +1,11 @@
 <script setup lang="ts">
-import Sidebar from '@/Components/Navigation/Sidebar.vue';
 import { ref, onMounted, onUnmounted } from 'vue';
 import HeaderMenu from '@/Components/Navigation/HeaderMenu.vue';
 import { sidebarMenuItems } from '@/config/navigation';
-import { Head } from '@inertiajs/vue3';
+import MainLayout from './MainLayout.vue';
 
-const mainContentHeight = ref<number>(0);
-const adminPage = ref<HTMLElement | null>(null);
+const contentHeight = ref<number>(0);
+const mainLayout = ref<HTMLElement | null>(null);
 const headerMenu = ref<HTMLElement | null>(null);
 const observer = ref<MutationObserver | null>(null);
 const currentPath = ref(window.location.pathname.split('/')[1]);
@@ -19,20 +18,20 @@ const getCurrentSubmenu = () => {
 const updateMainContentHeight = () => {
     requestAnimationFrame(() => {
         const headerMenuHeight = headerMenu.value?.getBoundingClientRect().bottom ?? 0;
-        mainContentHeight.value = window.innerHeight - headerMenuHeight - 80;
+        contentHeight.value = window.innerHeight - headerMenuHeight - 80;
     });
 };
 
 onMounted(() => {
-    headerMenu.value = adminPage.value!.querySelector('.header-menu');
+    headerMenu.value = mainLayout.value!.querySelector('.header-menu');
     
     window.addEventListener('resize', updateMainContentHeight);
     
     observer.value = new MutationObserver(() => {
-        setTimeout(() => updateMainContentHeight(), 1);
+        setTimeout(updateMainContentHeight, 1);
     });
     
-    observer.value.observe(adminPage.value!, {
+    observer.value.observe(mainLayout.value!, {
         attributes: true,
         subtree: true
     });
@@ -47,21 +46,18 @@ onUnmounted(() => {
 </script>
 
 <template>
-    <Head title="Calendrier prévisionnel" />
-    <div ref="adminPage" class="admin-page w-screen h-screen bg-gray-100 flex">
-        <Sidebar :items="sidebarMenuItems" />
-        <div class="w-full h-full">
+    <MainLayout>
+        <div ref="mainLayout" class="flex flex-col gap-10 -mt-10">
             <HeaderMenu :items="getCurrentSubmenu()!" />
-            <div class="main-content bg-red-900 m-10" :style="{ '--header-menu-height': mainContentHeight + 'px' }">
+            <div class="content w-full" :style="{ '--header-menu-height': contentHeight + 'px' }">
                 <slot />
             </div>
         </div>
-    </div>
+    </MainLayout>
 </template>
 
 <style scoped>
-.main-content {
-    width: calc(100% - 80px);
+.content {
     height: var(--header-menu-height);
 }
 </style>
