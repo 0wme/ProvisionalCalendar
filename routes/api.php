0b1017de579ae3ecec6c964f3f5db5214bdb193d
Smimid Sofian <sofian.smimid@etu.unilim.fr>
2024-11-18 17:19:19 +0100

    finishing of apis for teacher teachings page

--- routes/api.php
+++ routes/api.php
@@ -32,8 +32,28 @@ use App\Http\Controllers\Api\TeacherTeachingController;
 
 //Enseignants
 
+    //Récupération des données
+
     Route::get('/enseignants/{year}', [TeacherTeachingController::class, 'getTeachers']);
     Route::get('/enseignements/{year}', [TeacherTeachingController::class, 'getTeachings']);
     Route::get('/enseignement/enseignants/{teaching}', [TeacherTeachingController::class, 'getTeachersByTeaching']);
     Route::get('/enseignant/enseignements/{teacher}', [TeacherTeachingController::class, 'getTeachingsByTeacher']);
+    Route::get('/enseignant/{teacher}', [TeacherTeachingController::class, 'getTeacher']);
+    Route::get('/enseignement/{teaching}', [TeacherTeachingController::class, 'getTeaching']);
+
+    //Création des données
+
+    Route::post('/enseignant/{year}', [TeacherTeachingController::class, 'storeTeacher']);
+    Route::post('/enseignement/{year}', [TeacherTeachingController::class, 'storeTeaching']);
+
+    //Modification des données
+
+    Route::put('/enseignant/{teacher}', [TeacherTeachingController::class, 'updateTeacher']);
+    Route::put('/enseignement/{teaching}', [TeacherTeachingController::class, 'updateTeaching']);
+
+    //Suppression des données
+
+    Route::delete('/enseignant/{teacher}', [TeacherTeachingController::class, 'deleteTeacher']);
+    Route::delete('/enseignement/{teaching}', [TeacherTeachingController::class, 'deleteTeaching']);
     
+
