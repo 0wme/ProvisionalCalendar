aa12271b49674b62827ea933df5c050f8d4667db
Tom <93992802+0wme@users.noreply.github.com>
2024-11-28 10:59:53 +0100

    Add true data teaching

--- database/seeders/TeacherTeachingSeeder.php
+++ database/seeders/TeacherTeachingSeeder.php
@@ -12,6 +12,17 @@ class TeacherTeachingSeeder extends Seeder
     {
         $teacher = Teacher::where('acronym', 'LD')->first();
         $teaching = Teaching::where('apogee_code', 'WEB_R1.05')->first();
-        $teacher->teachings()->attach($teaching->id);
+    
+        if ($teacher && $teaching) {
+            $teacher->teachings()->attach($teaching->id);
+        } else {
+            // Log an error or handle the case where either $teacher or $teaching is null
+            if (!$teacher) {
+                echo "Teacher with acronym 'LD' not found.\n";
+            }
+            if (!$teaching) {
+                echo "Teaching with apogee_code 'WEB_R1.05' not found.\n";
+            }
+        }
     }
 }
