1642d483a87a3778b5e3a54179f122bc4e186010
Mael REINERT MARTINEZ <mael@reinertmartinez.fr>
2024-11-28 11:39:40 +0100

    refactoring TeachersListManager et TeachingsListManager

--- resources/js/Features/ListManager/TeachingsListManager.vue
+++ resources/js/Features/ListManager/TeachingsListManager.vue
@@ -1,52 +1,39 @@
 <script setup lang="ts">
 import ListManager from '@/Components/ListManager/ListManager.vue';
 import { Teaching, Period } from '@/types/models';
-import { defineProps } from 'vue';
-import AddTeachingPopup from '@/Components/Popup/AddTeachingPopup.vue';
-import EditTeachingPopup from '@/Components/Popup/EditTeachingPopup.vue';
-import { ref } from 'vue';
+import { defineProps, defineEmits } from 'vue';
 
 defineProps<{
-    title: string;
     periods: Period[];
     teachings: Teaching[];
+    selectedTeachingId?: number[];
 }>();
 
-const showPopup = ref(false);
-const showPopupEdit = ref(false);
-const openPopup = () => {
-    showPopup.value = true;
+const emit = defineEmits(['add', 'edit', 'select']);
+
+const handleSelect = (teaching: Teaching) => {
+    emit('select', teaching);
 }
 
 const handleEdit = (teaching: Teaching) => {
-    showPopupEdit.value = true;
+    emit('edit', teaching);
+}
+
+const handleAdd = () => {
+    emit('add');
 }
 </script>
 
 <template>
-    <div class="teachings-list-manager h-full w-full">
-        <ListManager
-            title="Enseignements"
-            hasAdd
-            hasImport
-            :periods
-            :items="teachings"
-            @add="openPopup"
-            @edit="handleEdit"
-        />
-        
-        <AddTeachingPopup 
-            class="z-50"
-            v-if="showPopup" 
-            :is-editing="false"
-            @close="showPopup = false" 
-        />
-
-        <EditTeachingPopup 
-            class="z-50"
-            v-if="showPopupEdit" 
-            :is-editing="true"
-            @close="showPopupEdit = false" 
-            />
-    </div>
+    <ListManager
+        title="Enseignements"
+        hasAdd
+        hasImport
+        :periods
+        :items="teachings"
+        :selectedItemsId="selectedTeachingId"
+        @add="handleAdd"
+        @edit="handleEdit"
+        @select="handleSelect"
+    />
 </template>
