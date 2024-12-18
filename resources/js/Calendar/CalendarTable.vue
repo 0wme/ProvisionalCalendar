31302ac1e49ee6cd2ecb526eb0ad6a02067d670e
Mael REINERT MARTINEZ <mael@reinertmartinez.fr>
2024-12-18 14:49:05 +0100

    adding of calendar table line

--- resources/js/Calendar/CalendarTable.vue
+++ resources/js/Calendar/CalendarTable.vue
@@ -1,185 +1,33 @@
 <script setup lang="ts">
-import {ref} from 'vue';
-import CalendarCell from '@/Components/CalendarCell.vue';
+import { ref } from "vue";
+import CalendarCell from "@/Components/CalendarCell.vue";
+import Data from "./data.json";
 
-const weeks = Array.from({length: 15}, (_, i) => i + 1);
+const weeksData = Data;
 
-const testData = ref([
-    {
-        week: 1,
-        promotion: {
-            cm: [
-                {
-                    hours: 1,
-                    teacherName: 'John Doe',
-                },
-                {
-                    hours: 2,
-                    teacherName: 'Jane Doe',
-                },
-            ]
-        },
-        groups: [
-            {
-                td: [
-                    {
-                        hours: 1,
-                        teacherName: 'John Doe',
-                    },
-                    {
-                        hours: 2,
-                        teacherName: 'Jane',
-                    },
-                ],
-                subgroups: [
-                    {
-                        tp: [
-                            {
-                                hours: 1,
-                                teacherName: 'John Doe',
-                            },
-                            {
-                                hours: 1,
-                                teacherName: 'Jane',
-                            },
-                        ],
-                    },
-                ],
-            },
-            {
-                td: [
-                    {
-                        hours: 3,
-                        teacherName: 'Jack Doe',
-                    },
-                    {
-                        hours: 2,
-                        teacherName: 'Jane',
-                    },
-                ],
-                subgroups: [
-                    {
-                        tp: [
-                            {
-                                hours: 1,
-                                teacherName: 'John Doe',
-                            },
-                            {
-                                hours: 1,
-                                teacherName: 'Jane',
-                            },
-                        ],
-                    },
-                    {
-                        tp: [
-                            {
-                                hours: 1,
-                                teacherName: 'John Doe',
-                            },
-                            {
-                                hours: 1,
-                                teacherName: 'Jane',
-                            },
-                        ],
-                    },
-                ],
-            },
-        ],
-    },
-    {
-        week: 2,
-        promotion: {
-            cm: [
-                {
-                    hours: 1,
-                    teacherName: 'John Doe',
-                },
-                {
-                    hours: 2,
-                    teacherName: 'Jane Doe',
-                },
-            ]
-        },
-        groups: [
-            {
-                td: [
-                    {
-                        hours: 1,
-                        teacherName: 'John Doe',
-                    },
-                    {
-                        hours: 2,
-                        teacherName: 'Jane',
-                    },
-                ],
-                subgroups: [
-                    {
-                        tp: [
-                            {
-                                hours: 1,
-                                teacherName: 'John Doe',
-                            },
-                            {
-                                hours: 1,
-                                teacherName: 'Jane',
-                            },
-                        ],
-                    },
-                ],
-            },
-            {
-                td: [
-                    {
-                        hours: 3,
-                        teacherName: 'Jack Doe',
-                    },
-                    {
-                        hours: 2,
-                        teacherName: 'Jane',
-                    },
-                ],
-                subgroups: [
-                    {
-                        tp: [
-                            {
-                                hours: 1,
-                                teacherName: 'John Doe',
-                            },
-                            {
-                                hours: 1,
-                                teacherName: 'Jane',
-                            },
-                        ],
-                    },
-                    {
-                        tp: [
-                            {
-                                hours: 1,
-                                teacherName: 'John Doe',
-                            },
-                            {
-                                hours: 1,
-                                teacherName: 'Jane',
-                            },
-                        ],
-                    },
-                ],
-            },
-        ],
-    }
-])
+console.log(weeksData);
 </script>
 
 <template>
-    <div class="tbody flex flex-col">
-        <div class="border-2 border-r-gray-200 border-b-gray-200 rounded-l-lg flex items-center justify-center p-4">S{{ week.week }}</div>
-        <div v-for="week in testData" :key="'week-' + week.week" class="flex">
-            <CalendarCell :title="`${cm.hours}h - ${cm.teacherName}`" v-for="cm in week.promotion"
-                           :key="'cm-' + cm.teacherName">
-                <CalendarCell :title="`TD${td}`" v-for="td in 3" :key="'td-' + week + '-' + td">
-                    <CalendarCell :title="`TP${tp}`" v-for="tp in 2" :key="'tp-' + week + '-' + tp"/>
-                </CalendarCell>
-            </CalendarCell>
+    <div class="thead flex">
+        <div class="thead flex flex-col">
+            <div
+                v-for="week in weeksData"
+                class="border-2 border-r-gray-200 border-b-gray-200 rounded-l-xl flex items-center justify-center p-4 h-52"
+            >
+                S {{ week.week }}
+            </div>
+        </div>
+        <div class="tbody flex flex-col h-full">
+            <div v-for="week in weeksData" class="flex h-full">
+                <div class="flex h-full">
+                    <CalendarCell
+                        class="h-52"
+                        v-for="group in week.groups"
+                        :group-data="group"
+                    />
+                </div>
+            </div>
         </div>
     </div>
 </template>
