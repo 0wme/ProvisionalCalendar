db567997864118f2b43eb961ba970f029b89c5ea
Mael REINERT MARTINEZ <mael@reinertmartinez.fr>
2024-12-20 22:06:09 +0100

    Enhances calendar sidebar structure and data handling
    Refactors sidebar to display nested group data per week
    Adds computed property to sum group hours dynamically
    Updates styling for improved layout and border alignment
    Adjusts sample data to reflect updated hour values

--- resources/js/Calendar/CalendarRightSidebar.vue
+++ resources/js/Calendar/CalendarRightSidebar.vue
@@ -7,7 +7,13 @@ defineProps<{
 </script>
 
 <template>
-    <div class="flex flex-col">
-        <RightSidebarElement v-for="week in weeksData" :week="week" />
+    <div class="flex flex-col border-l-2 border-gray-200">
+        <div v-for="week in weeksData" class="h-52 flex flex-col">
+            <RightSidebarElement
+                v-for="group in week.groups"
+                :group-data="group"
+                class="h-full"
+            />
+        </div>
     </div>
 </template>
--- resources/js/Calendar/RightSidebarElement.vue
+++ resources/js/Calendar/RightSidebarElement.vue
@@ -1,3 +1,31 @@
+<script setup lang="ts">
+import { computed, defineProps } from "vue";
+
+const props = defineProps<{
+    groupData: any;
+}>();
+
+const groupHoursSum = computed(() => {
+    if (!props.groupData?.contents) return 0;
+    return props.groupData.contents
+        .map((content: any) => content.hours)
+        .reduce((sum: number, hours: number) => sum + hours, 0);
+});
+</script>
+
 <template>
-    <div class="bg-blue-200 h-52 w-36 border-b-2 border-gray-200 test"></div>
+    <div class="flex w-max h-full">
+        <div
+            class="w-12 flex items-center justify-start bg-white border-r-2 border-b-2 border-gray-200"
+        >
+            <p>{{ groupHoursSum }}</p>
+        </div>
+        <div class="flex flex-col h-full">
+            <RightSidebarElement
+                v-for="group in groupData.groups"
+                :group-data="group"
+                class="h-full"
+            />
+        </div>
+    </div>
 </template>
