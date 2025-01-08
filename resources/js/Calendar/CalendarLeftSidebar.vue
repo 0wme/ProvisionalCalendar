7a252b18b84a2e6bde30a89791dbdd9e5898f576
Tom <93992802+0wme@users.noreply.github.com>
2025-01-08 13:45:01 +0100

    Improves calendar header styles and data structure
    Adds rounded corners to headers for enhanced UI consistency
    Introduces background color to left sidebar week elements
    Extends data structure to include additional group content details

--- resources/js/Calendar/CalendarLeftSidebar.vue
+++ resources/js/Calendar/CalendarLeftSidebar.vue
@@ -8,7 +8,7 @@ defineProps<{
     <div class="flex flex-col">
         <div
             v-for="week in weeksData"
-            class="border-2 border-r-gray-200 border-b-gray-200 rounded-l-xl flex items-center justify-center w-12 min-h-52"
+            class="border-2 border-r-gray-200 border-b-gray-200 rounded-l-xl flex items-center justify-center w-12 min-h-52 bg-[#FFE5DA]"
         >
             S {{ week.week }}
         </div>
