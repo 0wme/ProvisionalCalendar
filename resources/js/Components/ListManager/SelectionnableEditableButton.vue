a4953093d8c6f9570e9804fc5f60516838aa2014
Mael REINERT MARTINEZ <mael@reinertmartinez.fr>
2025-01-08 10:07:00 +0100

    Adds drag-and-drop support for list items
    Implements draggable functionality for selection buttons
    Enhances the SelectionnableEditableButton component by adding a
    drag event handler to include item data in drag-and-drop operations
    
    Improves code formatting and consistency across components

--- resources/js/Components/ListManager/SelectionnableEditableButton.vue
+++ resources/js/Components/ListManager/SelectionnableEditableButton.vue
@@ -1,27 +1,44 @@
 <script setup lang="ts">
-import SelectionnableButton from './SelectionnableButton.vue';
-import IconButton from '@/Components/IconButton.vue';
-import { Item } from '@/types/models';
+import SelectionnableButton from "./SelectionnableButton.vue";
+import IconButton from "@/Components/IconButton.vue";
+import { Item } from "@/types/models";
 
 const props = defineProps<{
-    item: Item,
-    selected?: boolean
-}>()
+    item: Item;
+    selected?: boolean;
+}>();
 
-const emit = defineEmits(['select', 'edit']);
+const emit = defineEmits(["select", "edit"]);
 
 const handleEdit = () => {
-    emit('edit', props.item.id);
-}
+    emit("edit", props.item.id);
+};
 
 const handleSelect = () => {
-    emit('select', props.item.id);
-}
+    emit("select", props.item.id);
+};
+
+const handleDrag = (event: DragEvent) => {
+    if (event.dataTransfer) {
+        event.dataTransfer.setData("text/plain", JSON.stringify(props.item));
+    }
+};
 </script>
 
 <template>
     <div class="selectionnable-button-editable flex items-center gap-2">
-        <SelectionnableButton class="w-full" :name="item.name" :selected @click="handleSelect" />
-        <IconButton iconClass="Pencil" bgColor="#E8DEF8" @click="handleEdit" hasShadow />
+        <SelectionnableButton
+            class="w-full"
+            :name="item.name"
+            :selected
+            @click="handleSelect"
+            @dragstart="handleDrag"
+        />
+        <IconButton
+            iconClass="Pencil"
+            bgColor="#E8DEF8"
+            @click="handleEdit"
+            hasShadow
+        />
     </div>
 </template>
